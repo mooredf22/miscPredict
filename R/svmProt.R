b91ae7433dc37f0036dfa7b1419b4d574b6d98e2
mooredf22 <dirkfmoore@gmail.com>
2024-12-11 13:49:20 -0500

    early stage development changes

---
+++ R/svmProt.R
@@ -0,0 +1,52 @@
+svmProt <- function(xx, yy) {
+  # xx is a matrix of samples with columns indicating protein signature names
+  # yy is a factor variable of 1's and 0's
+  # now set up for svm:
+  library(e1071)
+  library(pROC)
+  library(crossval)  # need for "confusionMatrix" and "diagnosticErrors"
+  x.orig <- as.data.frame(xx)
+  y.orig <- as.factor(yy)
+  nAll <- length(y.orig)  # number of observations
+
+  #model.orig <- svm(y.orig.f ~ ., probability=TRUE, data=x.orig)
+  model.orig <- svm(y.orig ~ ., probability=TRUE, cross=5, data=x.orig)
+  accuracy <- model.orig$tot.accuracy
+  pred <- predict(model.orig, newdata=x.orig, decision.values = FALSE, probability = TRUE)
+
+  # never do this!!
+  # predictedValues <- as.numeric(pred[1:nAll])
+
+  # see https://cran.r-project.org/doc/FAQ/R-FAQ.html#How-do-I-convert-factors-to-numeric_003f
+  if (length(levels(pred)) == 2) {
+    predictedValues <- as.numeric(levels(pred))[pred]
+  }
+  if (length(levels(pred)) > 2) {
+    predictedValues <- (levels(pred))[pred]
+  }
+
+
+  # these functions are from the "crossval" library:
+  cm <- confusionMatrix(a=y.orig, p=predictedValues, negative=0)
+  tempDiagErr <- diagnosticErrors(cm)
+  sens <- tempDiagErr[2]
+  spec <- tempDiagErr[3]
+
+  tableOut <- table(y.orig, predictedValues)
+
+  predMatrix <- attr(pred, "probabilities")
+  # column names of predMatrix should be 1 and 0
+  # otherwise predMatrix[,"1"] will not work!!
+  rocAll <- roc(response=y.orig,
+                predictor=predMatrix[,"1"], ci=TRUE,
+                smoothed=TRUE)
+  #pick off correct column, the one with column name "1";
+  # sometimes it is the first column, sometimes the second
+
+  aucOut <- as.numeric(auc(rocAll))
+  aucCI <- as.numeric(rocAll$ci)[c(1,3)]
+
+  result <- list(sens=sens, spec=spec, tableOut=tableOut,
+                 aucOut=aucOut, aucCI=aucCI, rocAll=rocAll)
+  result
+}
---
+++ R/svmProt2.R
@@ -0,0 +1,65 @@
+# # # # # # # # #
+# cross-validation of svm
+# # # # # # # # #
+
+
+
+svmProt2 <- function(XTrain, YTrain, XTest, YTest) {
+  library(crossval)
+
+  XTrain <- as.data.frame(XTrain)
+  XTest <- as.data.frame(XTest)
+  # xx is a matrix of samples with columns indicating protein signature names
+  # now set up for svm:
+  library(e1071)
+  library(pROC)
+  library(crossval)  # need for "confusionMatrix" and "diagnosticErrors"
+  #browser()
+  #x.orig <- xx
+  #y.orig <- as.factor(yy)
+  #nAll <- length(y.orig)  # number of observations
+
+  #model.orig <- svm(y.orig.f ~ ., probability=TRUE, data=x.orig)
+  model.orig <- svm(YTrain ~ ., probability=TRUE, cross=5, data=XTrain)
+  accuracy <- model.orig$tot.accuracy
+  names(accuracy) <- "accuracy"
+  pred <- predict(model.orig, newdata=XTest, decision.values = FALSE, probability = TRUE)
+
+  # never do this!!
+  # predictedValues <- as.numeric(pred[1:nAll])
+
+  # see https://cran.r-project.org/doc/FAQ/R-FAQ.html#How-do-I-convert-factors-to-numeric_003f
+  if (length(levels(pred)) == 2) {
+    predictedValues <- as.numeric(levels(pred))[pred]
+  }
+  if (length(levels(pred)) > 2) {
+    predictedValues <- (levels(pred))[pred]
+  }
+
+
+  # these functions are from the "crossval" library:
+  cm <- confusionMatrix(a=YTest, p=predictedValues, negative=0)
+  tempDiagErr <- diagnosticErrors(cm)
+  sens <- tempDiagErr[2]
+  spec <- tempDiagErr[3]
+
+  tableOut <- table(YTest, predictedValues)
+
+
+  rocAll <- roc(response=YTest,
+                predictor=attr(pred, "probabilities")[,"1"], ci=TRUE,
+                smoothed=TRUE)
+  #pick off correct column, the one with column name "1";
+  # sometimes it is the first column, sometimes the second
+
+  aucOut <- as.numeric(auc(rocAll))
+  names(aucOut) <- "AUC"
+  aucCI <- as.numeric(rocAll$ci)[c(1,3)]
+
+  #result <- list(sens=sens, spec=spec, tableOut=tableOut,
+  #aucOut=aucOut, aucCI=aucCI, rocAll=rocAll)
+  tableOutVec <- as.numeric(tableOut)  # make matrix into a vector
+  #names(tableOutVec) <- c("n11", "n21", "n12", "n22")
+  result <- c(accuracy, sens, spec, aucOut, tableOutVec)
+  result
+}
